d92cb2f9eb9ff6ddbc5419bc622957a6c2642955
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-12-06 13:59:17 -0500

    Updated from Brython Server: 12/6/2017 1:59:17 PM

--- longesDictionaryWord.py
+++ longesDictionaryWord.py
@@ -1,1 +1,14 @@
+#Clay Kynor
+#11/13/17
+#longestWord.py - finds the middle word
 
+dicctionary = open('engmix.txt')
+
+total = 0
+    
+for w in dicctionary:
+    num = len(w)
+    if num>total:
+        total = w
+
+print(total)
