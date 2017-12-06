d0973ecad5993e318bbfff1fe809931d00339cd4
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-12-06 13:59:29 -0500

    Updated from Brython Server: 12/6/2017 1:59:29 PM

--- fileDemo.py
+++ fileDemo.py
@@ -1,1 +1,13 @@
+#Clay Kynor
+#12/6/17
+#fileDemo.py - how to read a file
 
+dicctionary = open('engmix.txt')
+
+wordCount = 0
+for word in dicctionary:
+    if 'dab' in word:
+        print(word.strip())
+    wordCount += 1
+
+print('There are', wordCount, "words in the dicctionary")
