9ff4c288ca043070e0956ef24310b649a8ca52fd
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-12-07 14:44:49 -0500

    Updated from Brython Server: 12/7/2017 2:44:49 PM

--- longShort.py
+++ longShort.py
@@ -5,7 +5,12 @@
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','o','p','q','r','s','t','u','v','w','x','y','z']
 
 longest = 0
-word = ''
+word = ""
 
-while longest <= 25;
-
+for words in dictionary:
+    length = len(words)
+    if length > longest:
+        longest = length
+        word = words
+        
+print("The longest word is", word, "with", longest, "characters")
