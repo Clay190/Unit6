1cc050a5dd43aaf2adc01c7711558e3f2aade488
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-12-07 14:23:23 -0500

    Updated from Brython Server: 12/7/2017 2:23:23 PM

--- reverseFile.py
+++ reverseFile.py
@@ -1,1 +1,18 @@
+#Clay Kynor
+#12.7.17
+#reverseFile.py
 
+file = open(""+input('Enter a file: ')+"")
+
+l = []
+
+for line in file:
+    l.append(line)
+
+l.reverse()
+
+for i in l:        
+    print(i.strip())
+
+
+    
