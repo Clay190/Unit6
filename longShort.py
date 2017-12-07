504ac1fb024ea1073fa67f5504fb918b9a0449b2
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-12-07 14:43:34 -0500

    Updated from Brython Server: 12/7/2017 2:43:34 PM

--- longShort.py
+++ longShort.py
@@ -1,1 +1,11 @@
+#Clay Kynor
+#12.7.17
+#longestShort.py
+
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','o','p','q','r','s','t','u','v','w','x','y','z']
+
+longest = 0
+word = ''
+
+while longest <= 25;
 
