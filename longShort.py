b6e2fca2df85513d686c25b87d49bee430e933a5
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-12-07 14:49:21 -0500

    Updated from Brython Server: 12/7/2017 2:49:21 PM

--- longShort.py
+++ longShort.py
@@ -3,14 +3,21 @@
 #longestShort.py
 
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','o','p','q','r','s','t','u','v','w','x','y','z']
+dictionary = open("engmix.txt")
 
-longest = 0
-word = ""
+numlong = 0
+longest = ""
+shortest = ""
+numshort = 0
+
+longA = ''
+
+alphabet.index('b')
 
 for words in dictionary:
     length = len(words)
-    if length > longest:
-        longest = length
+    if length > numlong:
+        numlong = length
         word = words
         
-print("The longest word is", word, "with", longest, "characters")
+print("The longest word is", word, "with", numlong, "characters")
