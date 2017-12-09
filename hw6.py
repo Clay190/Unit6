8d483e9e5b55d3ea26386b048581299ba4cb5211
Clay190 <31509660+Clay190@users.noreply.github.com>
2017-12-09 18:30:20 -0500

    Updated from Brython Server: 12/9/2017 6:30:19 PM

--- hw6.py
+++ hw6.py
@@ -3,23 +3,45 @@
 #hw6.py
 
 dictionary = open('engmix.txt')
+
 '''
 word = input("Enter a word to check ")
 
-count = 0
+counter = 0
+
 for wrd in dictionary:
-    if wrd.strip() == word:
-        print(wrd, "is in the dictionary")
-        break
-    if count==0:
-        print("Your word is not in the dictionary")
-            
+    if counter == 0:
+        if wrd.strip() == word:
+            print(wrd, "is in the dictionary")
+            counter += 1
+            break
+if counter==0:
+    print("Your word is not in the dictionary")
 '''
 
+'''
 num = int(input("Enter the numbered word you would like to look for"))
 
 dictList = []
 
 for word in dictionary:
     dictList.append(word)
-print(dictList[num+1])
+print(dictList[num-1])
+'''
+'''
+file = open("input.txt")
+
+for line in file:
+    print(line.strip()+'!')
+'''
+
+letter = input("Enter a letter: ")
+    
+num = 0
+word = ""
+for words in dictionary:
+    repeat = words.count(letter)
+    if repeat > num:
+        num = repeat
+        word = words
+print(word)
